3bcf411057ab0d1771f5ddecda14b9e69ec861be
Christian Tietze <christian.tietze@gmail.com>
2017-07-08 07:28:28 +0200

    Initial Commit

---
+++ NSScrollViewCache/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSScrollViewCache
+//
+//  Created by Christian Tietze on 08.07.17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSScrollViewCache/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSScrollViewCache
+//
+//  Created by Christian Tietze on 08.07.17.
+//  Copyright © 2017 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
